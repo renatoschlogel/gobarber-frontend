7be3905751d1204af0b220a7cd309a041399387d
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-08-10 22:35:14 -0300

    Removendo console log

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -17,8 +17,6 @@ import { Container, Content, Background } from './SignUp.styles';
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  console.log(formRef);
-
   const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
