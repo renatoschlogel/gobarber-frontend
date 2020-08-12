bef44f4f14935db774364d004407bddc88c39f75
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-08-11 23:28:55 -0300

    Autenticaçao - API de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/signin/SignIn';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Renato' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,8 +1,10 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+
+import AuthContext from '../../context/AuthContext';
 
 import getValidationErros from '../../utils/getValidateErrors';
 
@@ -15,6 +17,9 @@ import { Container, Content, Background } from './SignIn.styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const auth = useContext(AuthContext);
+  console.log(auth);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
