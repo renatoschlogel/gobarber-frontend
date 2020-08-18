720c888d0418ee88ca5d5152750490481651763d
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-08-18 07:40:47 -0300

    Mensagem de toast - Criando hook de toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/signin/SignIn';
-import ToastContainer from './components/ToastContainer/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-
-    <ToastContainer />
+    </AppProvider>
 
     <GlobalStyle />
   </>
--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/authContext';
+import { useToast } from '../../hooks/toastContext';
 
 import getValidationErros from '../../utils/getValidateErrors';
 
@@ -24,6 +25,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -38,7 +40,7 @@ const SignIn: React.FC = () => {
 
         await schema.validate(data, { abortEarly: false });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
@@ -47,9 +49,11 @@ const SignIn: React.FC = () => {
           const errors = getValidationErros(error);
           formRef.current?.setErrors(errors);
         }
+
+        addToast();
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
