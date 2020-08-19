f4e1f9187ff58847247fa2ba8265921f03f83ec0
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-08-19 07:41:57 -0300

    Mensagem de toast - Isolando o componente de Toast

--- src/components/ToastContainer/ToastContainer.tsx
+++ src/components/ToastContainer/ToastContainer.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
 
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { Container, Toast } from './ToastContainer.styles';
-
-import { ToastMessage, useToast } from '../../hooks/toastContext';
+import Toast from './Toast/Toast';
+import { Container } from './ToastContainer.styles';
+import { ToastMessage } from '../../hooks/toastContext';
 
 interface ToastContainerData {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerData> = ({ messages }) => {
-  const { removeToast } = useToast();
-
   return (
     <Container>
       {messages.map(message => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button type="button" onClick={() => removeToast(message.id)}>
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
