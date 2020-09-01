fa7507e65dd3d33bc40dd6d304e610c396ae2156
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-09-01 01:20:19 -0300

    Mensagem de toast - Animando Toasts com react-spring

--- src/components/ToastContainer/ToastContainer.tsx
+++ src/components/ToastContainer/ToastContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import Toast from './Toast/Toast';
 import { Container } from './ToastContainer.styles';
@@ -9,10 +10,20 @@ interface ToastContainerData {
 }
 
 const ToastContainer: React.FC<ToastContainerData> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%' },
+      enter: { right: '0%' },
+      leave: { right: '-120%' },
+    },
+  );
+
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
       ))}
     </Container>
   );
