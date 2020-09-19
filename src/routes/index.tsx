a19fc634c2ef38fdf8d22c61d477088925971971
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-09-19 01:13:47 -0300

    Rotas da aplicaçao - Rotas Autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/dashboard/Dashboard';
 
 import SignIn from '../pages/signin/SignIn';
 import SignUp from '../pages/signup/SignUp';
+import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
